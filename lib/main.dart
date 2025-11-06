a3bcb52f07180c3e8c092b3fb8fef13a05cc6824
Tudor Sarghiuta <149654961+tudors2004@users.noreply.github.com>
2025-11-06 11:50:55 +0200

    readme and mockup screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class SkillSwapApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        fontFamily: 'Schyler',
       ),
       home: const PlaceholderHome(),
     );
