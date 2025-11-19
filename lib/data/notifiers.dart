920794253bf06bf4fbf4cf1bc80e087d8873b3ad
Tudor Sarghiuta <149654961+tudors2004@users.noreply.github.com>
2025-11-19 14:46:00 +0200

    added dependencies and created some files

--- lib/data/notifiers.dart
+++
@@ -1,7 +0,0 @@
-//ValueListenableBuilder to listen to page changes
-//ValueNotifier to hold the current page index(data)
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-ValueNotifier<int> selectedPageNotifier = ValueNotifier(0);
-ValueNotifier<bool> isDarkModeNotifier = ValueNotifier(true);
